7af01ca230fd1d60cf65bb773dbf5b3692fca79c
Luis Gerhorst <gerhorst@cs.fau.de>
2024-05-03 09:49:07 +0200

    debugging v2: tokio timeouts, disable dead code
    Revert part of "debugging: tokio timeouts, disable dead code"
    
    This reverts commit 26d2f2e67bcd03b4c291dee150fb4cadeeae76a7.

--- watchers/src/watchers.rs
+++ watchers/src/watchers.rs
@@ -20,6 +20,8 @@ use async_trait::async_trait;
 use std::{fmt::Display, sync::Arc};
 use tokio::time;
 
+use tokio::time::timeout;
+
 pub enum WatcherType {
     Idle,
     ActiveWindow,
@@ -79,18 +81,18 @@ async fn filter_first_supported(
 ) -> Option<Box<dyn Watcher>> {
     match watcher_type {
         WatcherType::Idle => {
-            watch!(create_watcher::<wl_ext_idle_notify::IdleWatcher>(
-                client,
-                "Wayland idle (ext-idle-notify-v1)"
-            ));
-            watch!(create_watcher::<wl_kwin_idle::IdleWatcher>(
-                client,
-                "Wayland idle (KDE)"
-            ));
-            watch!(create_watcher::<x11_screensaver_idle::IdleWatcher>(
-                client,
-                "X11 idle (screensaver)"
-            ));
+            // watch!(create_watcher::<wl_ext_idle_notify::IdleWatcher>(
+            //     client,
+            //     "Wayland idle (ext-idle-notify-v1)"
+            // ));
+            // watch!(create_watcher::<wl_kwin_idle::IdleWatcher>(
+            //     client,
+            //     "Wayland idle (KDE)"
+            // ));
+            // watch!(create_watcher::<x11_screensaver_idle::IdleWatcher>(
+            //     client,
+            //     "X11 idle (screensaver)"
+            // ));
             #[cfg(feature = "gnome")]
             watch!(create_watcher::<gnome_idle::IdleWatcher>(
                 client,
@@ -98,22 +100,22 @@ async fn filter_first_supported(
             ));
         }
         WatcherType::ActiveWindow => {
-            watch!(create_watcher::<
-                wl_foreign_toplevel_management::WindowWatcher,
-            >(
-                client,
-                "Wayland window (wlr-foreign-toplevel-management-unstable-v1)"
-            ));
-            // XWayland gives _NET_WM_NAME on some windows in KDE, but not on others
-            #[cfg(feature = "kwin_window")]
-            watch!(create_watcher::<kwin_window::WindowWatcher>(
-                client,
-                "KWin window (script)"
-            ));
-            watch!(create_watcher::<x11_window::WindowWatcher>(
-                client,
-                "X11 window"
-            ));
+            // watch!(create_watcher::<
+            //     wl_foreign_toplevel_management::WindowWatcher,
+            // >(
+            //     client,
+            //     "Wayland window (wlr-foreign-toplevel-management-unstable-v1)"
+            // ));
+            // // XWayland gives _NET_WM_NAME on some windows in KDE, but not on others
+            // #[cfg(feature = "kwin_window")]
+            // watch!(create_watcher::<kwin_window::WindowWatcher>(
+            //     client,
+            //     "KWin window (script)"
+            // ));
+            // watch!(create_watcher::<x11_window::WindowWatcher>(
+            //     client,
+            //     "X11 window"
+            // ));
             #[cfg(feature = "gnome")]
             watch!(create_watcher::<gnome_window::WindowWatcher>(
                 client,
@@ -130,7 +132,7 @@ pub async fn run_first_supported(client: Arc<ReportClient>, watcher_type: &Watch
     if let Some(mut watcher) = supported_watcher {
         info!("Starting {watcher_type} watcher");
         loop {
-            if let Err(e) = watcher.run_iteration(&client).await {
+            if let Err(e) = timeout(watcher_type.sleep_time(&client.config), watcher.run_iteration(&client)).await {
                 error!("Error on {watcher_type} iteration: {e}");
             }
             time::sleep(watcher_type.sleep_time(&client.config)).await;
